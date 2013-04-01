956749da23a5eaf34bbf571aa03133e847a1a9bb
Paul <paulmerlyn@sbcglobal.net>
2013-03-31 20:18:05 -0700

    Update destroyowner.php

--- destroyowner.php
+++ destroyowner.php
@@ -7,7 +7,7 @@ destroyowner.php lists all registered (i.e. full) and semi-registerd (i.e. Owner
 session_start();
 
 // Connect to DB
-$db = mysql_connect('localhost', 'paulme6_merlyn', 'fePhaCj64mkik')
+$db = mysql_connect('localhost', 'paulme6_merlyn', '')
 or die('Could not connect: ' . mysql_error());
 mysql_select_db('paulme6_abridg') or die('Could not connect to the abridg database: ' . mysql_error());
 
