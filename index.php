08da4f6888bb4bd9a106bd8e10587d3365242c2f
Paul <paulmerlyn@sbcglobal.net>
2013-03-31 20:15:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ ob_start();
 session_start();
 
 // Connect to database
-$db = mysql_connect('localhost', 'paulme6_merlyn', 'fePhaCj64mkik')
+$db = mysql_connect('localhost', 'paulme6_merlyn', '')
 or die('Could not connect: ' . mysql_error());
 mysql_select_db('paulme6_abridg') or die('Could not select database: ' . mysql_error());
 ?>
